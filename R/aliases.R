9ff19612b68ca76984a8f0ca531fa2f104b08954
Dirk Eddelbuettel <edd@debian.org>
2015-04-26 09:18:53 -0500

    initial commit

---
+++ R/aliases.R
@@ -0,0 +1,10 @@
+
+##' @title installGithub
+##' @param ... passed on to \code{install_github}
+##' @seealso \code{\link[devtools]{install_github}}
+installGithub <- function(...) install_github(...)
+
+##' @title buildPackage
+##' @param ... passed on to \code{build}
+##' @seealso \code{\link[devtools]{build}}
+buildPackage <- function(...) build(...)
